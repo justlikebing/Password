e029011af6a481a1d7fac4cfc02013922f872786
Anas Khan <89466320+justlikebing@users.noreply.github.com>
2023-02-20 19:51:38 +0530

    Add files via upload

---
+++ Password.java
@@ -0,0 +1,24 @@
+import java.security.SecureRandom;
+
+public class Password {
+    private static final String CHARACTERS = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789!@#$%^&*()-_=+[{]}\\|;:'\",<.>/?";
+
+    public static void main(String[] args) {
+        int passwordLength = 12; // Change this to set the desired password length
+        String password = generatePassword(passwordLength);
+        System.out.println("Suggested password: " + password);
+    }
+
+    public static String generatePassword(int length) {
+        SecureRandom random = new SecureRandom();
+        StringBuilder passwordBuilder = new StringBuilder(length);
+
+        for (int i = 0; i < length; i++) {
+            int randomIndex = random.nextInt(CHARACTERS.length());
+            char randomChar = CHARACTERS.charAt(randomIndex);
+            passwordBuilder.append(randomChar);
+        }
+
+        return passwordBuilder.toString();
+    }
+}
